2a411b9de571460ea72998eb91f19679b3bbe527
tudorfis <todorescu.consulting@gmail.com>
2020-07-19 17:24:15 +0300

    practice environment

---
+++ src/app-16/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <top />
+    <left />
+    <bottom />
+  </div>
+</template>
+<script>
+import TopVue from './components/Top.vue';
+import LeftVue from './components/Left.vue';
+import BottomVue from './components/Bottom.vue';
+export default {
+  components: {
+    top: TopVue,
+    left: LeftVue,
+    bottom: BottomVue
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,5 @@
-
-const appNumber = window.location.search.match(/\=(\d+)/)[1];
-
 import Vue from 'vue'
-import App14 from './app-14/App.vue'
-import App15 from './app-15/App.vue'
-
-let App
-switch(appNumber) {
-  case '14': App = App14; break;
-  case '15': App = App15; break;
-}
+import App from './app-16/App.vue'
 
 const app = new Vue({
   el: '#app',
